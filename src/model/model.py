c2b8cee27d44c9fecafd6b988b7fad23f294e262
DESKTOP-BE2SPQH\duxin <duxin_be@outlook.com>
2018-09-04 20:51:02 +0900

    INITIAL COMMIT

---
+++ src/model/model.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-  
+# Created Date: Saturday September 1st 2018
+# Author: duxin
+# Email: duxin_be@outlook.com
+# Github: @Dosann
+# -------------------------------
+
+class SequenceSolver:
+
+    def __init__(): # TODO
+        pass
+    
+    def fit(self, X, y=None): # TODO
+        pass
+    
+    
+
+
+
