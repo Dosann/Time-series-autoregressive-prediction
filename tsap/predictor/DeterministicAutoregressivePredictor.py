cfa1f9981b95420fb17fb86683c17f2e0449f9cc
duxin <>
2018-10-17 23:55:40 +0900

    debug

--- tsap/predictor/DeterministicAutoregressivePredictor.py
+++ tsap/predictor/DeterministicAutoregressivePredictor.py
@@ -149,6 +149,8 @@ class DetermDiscreteAGPredictorStateful(Predictor):
         # return :
         #   prob : (input_size, n_classes)
         #   pred : (input_size)
+        print("determ.before solver.predict(X). solver's input_shape: ", solver._solver.input_shape)
+        print("shape of X: ", X.shape)
         prob = solver.predict(X[np.newaxis,...]) # [(1, n_classes)] or (1, n_classes)
         if type(prob) is list:
             prob = np.concatenate(prob, axis=0) # (input_size, n_classes)
